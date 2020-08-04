e7241d473f8da90906406e3a479cd94685ee498a
Richard Piper <dr.rd.piper@gmail.com>
2020-08-04 15:05:40 +1000

    Minor changes

--- Build-Research-Guide.R
+++ Build-Research-Guide.R
@@ -4,3 +4,4 @@ system("scp -r -i ~/.ssh/google_compute_engine  ./_book/* rpiper@surgery.rnsh.or
 bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::gitbook')
 bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::word_document2')
 bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::epub_book')
+bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::pdf_book')
