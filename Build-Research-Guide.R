efdd4a0ed3408f0d75c63b1e251a01c260e9f9d2
Richard Piper <dr.rd.piper@gmail.com>
2020-08-04 13:38:32 +1000

    Original

---
+++ Build-Research-Guide.R
@@ -0,0 +1,6 @@
+# Copy files to web server
+system("scp -r -i ~/.ssh/google_compute_engine  ./_book/* rpiper@surgery.rnsh.org:/var/www/html/research-guide/_book")
+# Make each of the outputs
+bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::gitbook')
+bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::word_document2')
+bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::epub_book')
