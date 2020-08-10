77e67558cb8bd32a53c6b414f0b6ba1af9dc7601
Richard Piper <dr.rd.piper@gmail.com>
2020-08-11 09:51:17 +1000

    Piper

--- Build-Research-Guide.R
+++ Build-Research-Guide.R
@@ -1,5 +1,9 @@
 # Copy files to web server
 system("scp -r -i ~/.ssh/google_compute_engine  ./_book/* rpiper@surgery.rnsh.org:/var/www/html/research-guide/_book")
+system("scp -r -i ~/.ssh/google_compute_engine  ./images/* rpiper@surgery.rnsh.org:/var/www/html/research-guide/images")
+system("scp -r -i ~/.ssh/google_compute_engine  ./_audio/* rpiper@surgery.rnsh.org:/var/www/html/research-guide/_audio")
+
+
 # Make each of the outputs
 bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::gitbook')
 bookdown::render_book(input = "index.Rmd", output_format = 'bookdown::word_document2')
